4ce45fa1549b3bb47b4c5a5ee3c69c12e56821cc
Elias Mawa <emawa@sawol.moe>
2018-10-12 13:21:52 -0600

    portfolio and portal controller add

--- routes/index.ts
+++ routes/index.ts
@@ -2,3 +2,5 @@ export * from "./css.controller";
 export * from "./index.controller";
 export * from "./projects.controller";
 export * from "./about.controller";
+export * from "./portfolio.controller";
+export * from "./portal.controller";
