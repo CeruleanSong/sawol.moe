8099d9cf111b3a0c9cdb05308306de0e34f01c11
Elias Mawa <elias@emawa.io>
2019-12-09 06:44:40 +0000

    update

---
+++ build.sh
@@ -0,0 +1,24 @@
+# build.sh - utlities for building and running kurisubot
+#
+# ./build.sh build - transpile code, and move to the build/ dir
+# ./build.sh run - run kurisubot
+#
+
+#if [ "$1" = "build"] # transpile the typescript
+#	tsc
+#	# create dir if not exists then copy transpiled files
+#	mkdir -p build/data && cp -R src/data build/
+#then
+
+if [ "$1" = "run" ] # run program
+then
+	# move to bot dir and run bot
+	cd dist && node index.js
+else
+	echo "compiling"
+	tsc
+
+	echo "moving to files to build/ directory"
+	# create dir if not exists then copy transpiled files
+	cp -R src/views dist/src/views && cp -R src/public dist/src/
+fi
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./css.controller"));
+__export(require("./index.controller"));
+__export(require("./projects.controller"));
+__export(require("./about.controller"));
+__export(require("./portfolio.controller"));
+__export(require("./portal.controller"));
